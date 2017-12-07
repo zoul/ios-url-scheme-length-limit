411a4dae7fda7ceeab61eea874161425132f65e3
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2017-12-07 11:28:09 +0100

    Use random string instead of repeated character

--- Transmitter/ViewController.swift
+++ Transmitter/ViewController.swift
@@ -9,12 +9,13 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         let testURL = URL(string: "receiver://test")!
         button.isEnabled = UIApplication.shared.canOpenURL(testURL)
+        print(randomStringOfLength(100))
     }
 
     @IBAction func transmit() {
         let sizes: [Int] = [1, 100, 1000, 100_000, 1000_000]
         let dataSize = sizes[sizeSelector.selectedSegmentIndex]
-        let payload = String(repeating: "x", count: dataSize)
+        let payload = randomStringOfLength(dataSize)
         guard let url = URL(string: "receiver://\(payload)") else {
             showError("Failed to create the URL.")
             return
@@ -30,5 +31,16 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
+    }
+
+    private func randomStringOfLength(_ len: Int) -> String {
+        let chars = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        let charCount = UInt32(chars.count)
+        var out = ""
+        for _ in 0 ..< len {
+            let i = Int(arc4random_uniform(charCount))
+            out += String(chars[i])
+        }
+        return out
     }
 }
